98ff74cab8fa0996e420858400322831641565ed
tomasOndra <ondra_tomas@oauh.cz>
2025-05-09 13:18:27 +0200

    Kvízy

---
+++ src/KvizGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class KvizGUI {
+    private JTextArea textArea1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+}
+
